aad81dd9c346eefc573012cf7b60a4ad38777a35
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-03-30 13:20:09 -0300

    styles and header finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './styles/App.css'
 import Header from './components/header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <FontAwesomeIcon icon="fa-solid github" size='40px' color='black'/>
     </>
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,105 +1,83 @@
-
-import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-import { Box } from '@mui/material';
-import Tooltip from '@mui/material';
-import Avatar from '@mui/material';
-import IconButton from '@mui/material';
-import Menu from '@mui/material';
-import MenuItem from '@mui/material/MenuItem';
-import Typography from '@mui/material/Typography';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faCartShopping, faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Header.css'
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const user = "User"
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
-    borderRadius: theme.shape.borderRadius,
+    borderRadius: theme,
     backgroundColor: '#fff',
-    '&:active': {
-      backgroundColor: '#d1d1d1',
-    },
+    border: '1px solid 021A32',
     marginLeft: 0,
     width: '100%',
-    height: '30px'
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 1),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    maxWidth: '600px',
+    height: '40px',
+    '&:focus': {
+        border: '3px solid #2870EA',
+        fontWeight: 400,
+    },
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
     width: '100%',
-    fontSize: '11px',
+    fontSize: '15px',
     '& .MuiInputBase-input': {
         padding: theme.spacing(1, 1, 1, 0),
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        paddingLeft: `calc(1em + ${theme.spacing(1)})`,
     },
 }));
 
 function Header() {
-
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
 
     return(
         <div className="header">
             <div className="content-top">
                 <div className="icon"></div>
                 <Search>
-                    <SearchIconWrapper>
-                        <SearchIcon />
-                    </SearchIconWrapper>
                     <StyledInputBase
                     placeholder="search for products, brands and more..."
                     inputProps={{ 'Helvetica': 'search' }}
                     />
+                    <button className='button-search'>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className='search'/>
+                    </button>
                 </Search>
-                <Box sx={{ flexGrow: 0 }}>
-                    <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                    </IconButton>
-                    </Tooltip>
-                    <Menu
-                    sx={{ mt: '45px' }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
-                    >
-                    {settings.map((setting) => (
-                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                        <Typography textAlign="center">{setting}</Typography>
-                        </MenuItem>
-                    ))}
-                    </Menu>
-                </Box>
+                <div className="right-content">
+                    <div className='cart'>
+                        <FontAwesomeIcon icon={faCartShopping} className="highlight"/>
+                    </div>
+                    <div className="user-botton">
+                        <button>
+                            <FontAwesomeIcon icon={faUser} className="highlight" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="content-bottom">
+                <button className="location">
+                    <div>
+                        <FontAwesomeIcon icon={faLocationDot} fontSize='20px'/>
+                        <span>
+                            <h4>{`Sent to ${user}.`}</h4>
+                            <p>Happy Street - 123</p>
+                        </span>
+                    </div>
+                </button>
+                <div className="nav-buttons">
+                    <a href="#">Categorias</a>
+                    <a href="#">Categorias</a>
+                    <a href="#">Categorias</a>
+                    <a href="#">Categorias</a>
+                    <a href="#">Categorias</a>
+                    <a href="#">Categorias</a>
+                    <a href="#">Categorias</a>
+                    <a href="#">Categorias</a>
+                </div>
+
             </div>
         </div>
     )
