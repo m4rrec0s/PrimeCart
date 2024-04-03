45f8dc05733a497b51078f6a07036babeee8ec1b
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-04-03 12:24:56 -0300

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,38 @@
+import { useState, useEffect } from 'react';
 import Banner from "../components/banner";
 import '../styles/Home.css'
 
 function Home() {
+    const [gradientTop, setGradientTop] = useState(130);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const windowWidth = window.innerWidth;
+            let gradientTopValue;
+
+            if (windowWidth <= 1130) {
+                gradientTopValue = 300;
+            } else {
+                gradientTopValue = Math.max(0, windowWidth - 725) * 0.30 + 130;
+            }
+    
+            setGradientTop(gradientTopValue);
+        };
+    
+        window.addEventListener('resize', handleResize);
+  
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+        }, []
+    );
+
     return (
+
         <>
             <div className="container-center">
                 <Banner />
-                <div className="content">
+                <div className="content" style={{ top: gradientTop }}>
                     <div className="center">
                         <section id="part-1">
                             <div className="recents">
